97f52a43069ec41c0a30423f9f7453148657b884
sergey <he305@mail.ru>
2023-04-19 22:23:42 +0300

    added tests

---
+++ test/Domain.Tests/Model/Values/ChatInfoTests.cs
@@ -0,0 +1,31 @@
+﻿using Xunit;
+using Domain.Model.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Exceptions;
+
+namespace Domain.Model.Values.Tests
+{
+    public class ChatInfoTests
+    {
+        [Fact()]
+        public void ChatInfoChatIdEmpty()
+        {
+            string chatId = "";
+
+            Assert.Throws<StringValidationException>(() => new ChatInfo(chatId));
+        }
+
+        [Fact()]
+        public void ChatInfoOk() 
+        {
+            string chatId = "some";
+
+            var exception = Record.Exception(() => new ChatInfo(chatId));
+            Assert.Null(exception);
+        }
+    }
+}
---
+++ test/Domain.Tests/Model/Values/UserInfoTests.cs
@@ -0,0 +1,45 @@
+﻿using Xunit;
+using Domain.Model.Values;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Common.Exceptions;
+
+namespace Domain.Model.Values.Tests
+{
+    public class UserInfoTests
+    {
+        [Fact()]
+        public void UserInfoNameEmpty()
+        {
+            string name = "";
+            AnimeListInfo animeListInfo = new AnimeListInfo("s", "s");
+
+            Assert.Throws<StringValidationException>(() => new UserInfo(name, animeListInfo));
+        }
+
+        [Fact()]
+        public void UserInfoOk()
+        {
+            string name = "name";
+            AnimeListInfo animeListInfo = new AnimeListInfo("s", "s");
+
+            var exception = Record.Exception(() => new UserInfo(name, animeListInfo));
+            Assert.Null(exception);
+        }
+
+        [Fact()]
+        public void GetAtomicValuesTest()
+        {
+            string name = "name";
+            AnimeListInfo animeListInfo = new AnimeListInfo("s", "s");
+            UserInfo userInfo = new UserInfo(name, animeListInfo);
+
+            object[] expected = { name, animeListInfo};
+
+            Assert.True(userInfo.GetAtomicValues().Take(2).SequenceEqual(expected));
+        }
+    }
+}
