148aa013924ff797eb5944fd78fd4fa6c4e54edd
sergey <he305@mail.ru>
2023-04-19 22:23:24 +0300

    added domain layer

---
+++ src/Domain/Model/Values/UserInfo.cs
@@ -0,0 +1,24 @@
+﻿using Common.Primitives;
+using Common.Validators;
+
+namespace Domain.Model.Values;
+
+public class UserInfo : ValueObject
+{
+    public string Name { get; set; }
+    public AnimeListInfo AnimeListInfo{ get; set; }
+
+    public UserInfo(string name, AnimeListInfo animeListInfo) 
+    {
+        StringValidator.Validate(name);
+
+        Name = name;
+        AnimeListInfo = animeListInfo;
+    }
+
+    public override IEnumerable<object> GetAtomicValues()
+    {
+        yield return Name;
+        yield return AnimeListInfo;
+    }
+}
